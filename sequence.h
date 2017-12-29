dd099eac3dd08f29b2febcc594d78a0a4fe38da2
Luka Novak <novak.luka93@gmail.com>
2017-12-29 22:01:29 +0100

    Modify sequence header and implement it

--- sequence.h
+++ sequence.h
@@ -8,6 +8,15 @@ class Sequence
         std::string sequence;
         Sequence();
     public:
-        static Sequence createFromFile(std::string);
+        Sequence(int, int, std::string);
+        static Sequence createFromFile();
+ 
+        int getBackboneIndex()      {return backboneIndex;}
+        int getsequenceIndex()      {return sequenceIndex;}
+        std::string getSequence()   {return sequence;}
+ 
+        void saveToFile(std::string);
+        Sequence loadFromFile(std::string, std::string);
+       
         ~Sequence();
 };
