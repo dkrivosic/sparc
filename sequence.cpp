e694f6fc9010c6ef7a7d350d627899b95553b02a
lukanovak93 <novak.luka93@gmail.com>
2018-01-08 17:22:13 +0100

    Add cigarSequence attribute, fix save and load functions, other fixes

--- sequence.cpp
+++ sequence.cpp
@@ -2,30 +2,47 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include <sstream>
 #include <vector>
 
-Sequence::Sequence(int bIndex, int seqIndex, std::string seq) {
+Sequence::Sequence(int bIndex, int seqIndex, std::string cigar_seq, std::string seq) {
     backboneIndex = bIndex;
     sequenceIndex = seqIndex;
+    cigar_sequence = cigar_seq;
     sequence = seq;
 }
 
-static Sequence createFromFile() {
+Sequence Sequence::createFromFile() {
     // create folder for sequence files 
-    system("mkdir /home/luka/Workspaces/C++/sparc/sequence_alignments");
+    std::string folder = "/home/luka/Workspaces/C++/sparc/sequence_alignments";
+
+    const int dir_err = system("mkdir -p /home/luka/Workspaces/C++/sparc/sequence_alignments");
+    if (-1 == dir_err)
+    {
+        printf("Error creating directory!n");
+        exit(1);
+    }
     
     // open alignments SAM file created GraphMap with CIGAR syntax
     std::string line;
-    std::ifstream alignmentsFile;
-    alignmentsFile.open ("/home/luka/Workspaces/C++/sparc/consenus_input/lambda_alignments_cigar.sam");
+    std::ifstream alignmentsFile("/home/luka/Workspaces/C++/sparc/consenus_input/lambda_alignments_cigar.sam");
+    // alignmentsFile.open ("/home/luka/Workspaces/C++/sparc/consenus_input/lambda_alignments_cigar.sam");
 
     if (alignmentsFile.is_open()){
         std::vector<std::string> tokens;
         std::stringstream stringToInt;
-        while (!alignmentsFile.eof){
-            // read the line from the file
+        int backboneIndexInt;
+        int a;
+        std::string b;
+        int sequenceIndexInt;
+        std::string cigarSequence;
+        std::string sequence;
+        while (alignmentsFile >> sequenceIndexInt >> a >> b >> backboneIndexInt >> a >> cigarSequence >> sequence >> a >> a >> b >> b >> b >> b >> b >> b >> b >> b >> b >> b){
+            
+            Sequence::saveToFile(folder, sequenceIndexInt, backboneIndexInt,cigarSequence, sequence);
+
+            /*// read the line from the file
             getline(alignmentsFile, line);
             
             // split it by tab character
@@ -36,7 +53,7 @@ static Sequence createFromFile() {
             stringToInt >> tokens[3];
             int sequenceIndexInt = 0;
             stringToInt >> tokens[0];
-            Sequence seq (backboneIndexInt, sequenceIndexInt, tokens[5]);
+            Sequence *seq = new Sequence(backboneIndexInt, sequenceIndexInt, tokens[5]);*/
 
             // save to file
         }
@@ -49,21 +66,35 @@ static Sequence createFromFile() {
     alignmentsFile.close();
 }
 
-void Sequence::saveToFile(std::string saveLocation){
-    int index = getsequenceIndex();
-    std::string filePath = saveLocation + std::to_string(index) + ".txt";
+void Sequence::saveToFile(std::string saveLocation, int sequenceIndexInt, int backboneIndexInt,std::string cigarSequence, std::string sequence){
+    std::string filePath = saveLocation + std::to_string(sequenceIndexInt) + ".txt";
     std::ofstream ofsSaveSequence(filePath.c_str());
-    ofsSaveSequence << backboneIndex << std::endl <<sequenceIndex << std::endl << sequence;
+    if (ofsSaveSequence.is_open()){
+        ofsSaveSequence << sequenceIndexInt << std::endl << backboneIndexInt << std::endl << cigarSequence << std::endl << sequence;
+    }
+    ofsSaveSequence.close();
 }
 
 Sequence Sequence::loadFromFile(std::string loadLocation, std::string index){
     std::string loadPath = loadLocation + index + ".txt";
+    std::string line;
     std::ifstream ifsLoadSequence(loadPath.c_str());
-    ifsLoadSequence >> backboneIndex >> sequenceIndex >> sequence;
-     // create object and return
+    int backboneIndex;
+    int sequenceIndex;
+    std::string cigarSequence;
+    std::string sequence;
+    if (ifsLoadSequence.is_open()){
+        ifsLoadSequence >> backboneIndex >> sequenceIndex >> cigarSequence >> sequence;
+        /*ifsLoadSequence >> backboneIndex;
+        ifsLoadSequence >> sequenceIndex;
+        ifsLoadSequence >> sequence;*/
+        ifsLoadSequence.close();
+    }
+     // create object and return it
+    Sequence *seq = new Sequence(backboneIndex, sequenceIndex, cigarSequence, sequence);
 }
 
-std::vector<std::string> split(const std::string& s, char delimiter)
+/*std::vector<std::string> split(const std::string& s, char delimiter)
 {
    std::vector<std::string> tokens;
    std::string token;
@@ -73,4 +104,4 @@ std::vector<std::string> split(const std::string& s, char delimiter)
       tokens.push_back(token);
    }
    return tokens;
-}
+}*/
