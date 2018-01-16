7573b6aa2c55c563ed3c240d17ac2630fcb0939d
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-16 16:24:11 +0100

    Remove comments

--- Sparc.cpp
+++ Sparc.cpp
@@ -12,35 +12,35 @@
 int main()
 {
 
-    // std::string pathToAlignments = "consensus_input/lambda_alignments_cigar.sam"; // path to location folder of the alignments.sam file
-    // std::cout << "Extracting sequences started from file: '" + pathToAlignments + "'"<< std::endl;
-    // Sequence::extractSequences(pathToAlignments);
-    // std::cout << "\nFinished! Exiting..." << std::endl;
-    // Backbone* bb = Backbone::createFromFile("consensus_input/lambda_layout.fasta");
-    // KmerGraph *graph = new KmerGraph(bb, 2, 3);
+    std::string pathToAlignments = "consensus_input/lambda_alignments_cigar.sam"; // path to location folder of the alignments.sam file
+    std::cout << "Extracting sequences started from file: '" + pathToAlignments + "'"<< std::endl;
+    Sequence::extractSequences(pathToAlignments);
+    std::cout << "\nFinished! Exiting..." << std::endl;
+    Backbone* bb = Backbone::createFromFile("consensus_input/lambda_layout.fasta");
+    KmerGraph *graph = new KmerGraph(bb, 2, 3);
 
-    // std::string folder = "sequence_alignments";
+    std::string folder = "sequence_alignments";
     
-    // int count = 0;
-    // DIR* dirp = opendir(folder.c_str());
-    // struct dirent * dp;
-    // while ((dp = readdir(dirp)) != NULL) 
-    // {
-    //     std::string f = folder + "/" + (dp->d_name);
-    //     std::cout << "File: " << f << "\n" << std::endl;
-    //     if(f.substr(f.length() - 3) == "txt")
-    //     {
-    //         Sequence *sequence = Sequence::loadFromFile(f);
-    //         graph->addSequence(sequence);    
-    //         count++;
+    int count = 0;
+    DIR* dirp = opendir(folder.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != NULL) 
+    {
+        std::string f = folder + "/" + (dp->d_name);
+        // std::cout << "File: " << f << "\n" << std::endl;
+        if(f.substr(f.length() - 3) == "txt")
+        {
+            Sequence *sequence = Sequence::loadFromFile(f);
+            graph->addSequence(sequence);    
+            count++;
    
-    //     }
-    // }
-    // closedir(dirp);
+        }
+    }
+    closedir(dirp);
 
-    // std::cout << "\nNumber of files read: " << count << "\n" << std::endl;
+    std::cout << "\nNumber of files read: " << count << "\n" << std::endl;
 
-    TopologicalSortSearch *tss = new TopologicalSortSearch(TopologicalSortSearch::createGraph());
+    TopologicalSortSearch *tss = new TopologicalSortSearch(graph->getGraph());
     std::pair<std::string, int> solution = tss->run();
     std::string path = solution.first;
     int weight = solution.second;
