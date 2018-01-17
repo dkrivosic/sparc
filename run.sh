d574ee8b2fae84c9b4ff17fa80a9792bf8380c47
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-17 15:32:25 +0100

    Add script for running sparc algorithm

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 layout_file="consensus_input/$1_layout.fasta"
-alignmnets_file="consensus_input/$1_alignments_cigar.sam"
+alignments_file="consensus_input/$1_alignments_cigar.sam"
 result_file="consensus_input/$1_results.fasta"
 reads_file="consensus_input/$1_reads.fastq"
 reference_file="consensus_input/$1_reference.fasta"
 
 g++ -std=c++11 Sparc.cpp Node.cpp Sequence.cpp KmerGraph.cpp Edge.cpp backbone.cpp Insert.cpp TopologicalSortSearch.cpp -o main
 
+dnadiff $reference_file $layout_file &> /dev/null
+res=$(cat out.report | head -19 | tail -1 | grep -Eo '[0-9\.]+$')
+echo "Initial accuracy: $res"
+
 for (( i=1; i<=$2; i++))
 	do
-        echo "Iteration $i"
+        echo "\nIteration $i"
         echo "Aligning reads to the layout."
-        /Users/domagoj/Downloads/graphmap/bin/mac/graphmap align -r $layout_file -d $reads_file -o $alignmnets_file --extcigar &> /dev/null
+        /Users/domagoj/Downloads/graphmap/bin/mac/graphmap align -r $layout_file -d $reads_file -o $alignments_file --extcigar &> /dev/null
         echo "Reads aligned."
-		./main $layout_file $alignmnets_file $alignmnets_file $result_file
+		./main $layout_file $alignments_file $result_file
         layout_file=$result_file
 	done
 
 dnadiff $reference_file $result_file &> /dev/null
-
 res=$(cat out.report | head -19 | tail -1 | grep -Eo '[0-9\.]+$')
 echo "Accuracy: $res"
 
