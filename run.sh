c098bdc668df279eb4dbaacbf89b1d25359467bc
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-17 15:51:26 +0100

    Add platform detection

--- run.sh
+++ run.sh
@@ -10,11 +10,20 @@ dnadiff $reference_file $layout_file &> /dev/null
 res=$(cat out.report | head -19 | tail -1 | grep -Eo '[0-9\.]+$')
 echo "Initial accuracy: $res"
 
+
+platform='mac'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='Linux-x64'
+fi
+
 for (( i=1; i<=$2; i++))
 	do
         echo "\nIteration $i"
         echo "Aligning reads to the layout."
-        graphmap/bin/mac/graphmap align -r $layout_file -d $reads_file -o $alignments_file --extcigar &> /dev/null
+
+
+        "graphmap/bin/$platform/graphmap" align -r $layout_file -d $reads_file -o $alignments_file --extcigar &> /dev/null
         echo "Reads aligned."
 		./main $layout_file $alignments_file $result_file
         layout_file=$result_file
