f7fe14b900aa134b4bed016e3c5dada7a837912f
Luka Novak <novak.luka93@gmail.com>
2018-01-17 17:07:25 +0100

    Add bash env to run.sh and fix unzip in prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,7 +1,7 @@
 # Download and extract data
 wget https://www.dropbox.com/s/a40dhhfchojyf0c/consenus_input.zip?dl=0
-unzip consenus_input.zip
-rm consenus_input.zip
+unzip consenus_input.zip?dl=0 -d consensus_input
+rm consenus_input.zip?dl=0
 
 # Install graphmap
 git clone https://github.com/isovic/graphmap.git
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 layout_file="consensus_input/$1_layout.fasta"
 alignments_file="consensus_input/$1_alignments_cigar.sam"
 result_file="consensus_input/$1_results.fasta"
@@ -13,15 +15,15 @@ echo "Initial accuracy: $res"
 
 platform='mac'
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
+if [[ "$unamestr" == 'Linux' ]] 
+then
    platform='Linux-x64'
 fi
 
 for (( i=1; i<=$2; i++))
 	do
-        echo "\nIteration $i"
+        echo "Iteration $i"
         echo "Aligning reads to the layout."
-
 
         "graphmap/bin/$platform/graphmap" align -r $layout_file -d $reads_file -o $alignments_file --extcigar &> /dev/null
         echo "Reads aligned."
